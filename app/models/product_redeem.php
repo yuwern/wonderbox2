15b9901f9a4ec507b7c081dc1665d5026dd08771
unknown <suresh.s@.openwave.com>
2013-01-25 15:49:03 +0530

    latest file is committed

---
+++ app/models/product_redeem.php
@@ -0,0 +1,52 @@
+<?php
+class ProductRedeem extends AppModel
+{
+    public $name = 'ProductRedeem';
+    //$validate set in __construct for multi-language support
+    //The Associations below have been created with all possible keys, those that are not needed can be removed
+    public $belongsTo = array(
+        'Product' => array(
+            'className' => 'Product',
+            'foreignKey' => 'product_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'counterCache' => true
+        ) ,
+        'User' => array(
+            'className' => 'User',
+            'foreignKey' => 'user_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => '',
+            'counterCache' => false
+        )
+    );
+    function __construct($id = false, $table = null, $ds = null)
+    {
+        parent::__construct($id, $table, $ds);
+        $this->validate = array(
+            'product_id' => array(
+                'rule1' => array(
+                    'rule' => 'numeric',
+                    'allowEmpty' => false,
+                    'message' => __l('Required') ,
+                ) ,
+            ) ,
+            'user_id' => array(
+                'rule1' => array(
+                    'rule' => 'numeric',
+                    'allowEmpty' => false,
+                    'message' => __l('Required') ,
+                ) ,
+            ) ,
+            'is_redeem' => array(
+                'rule1' => array(
+                    'rule' => 'boolean',
+                    'allowEmpty' => false,
+                    'message' => __l('Required') ,
+                ) ,
+            ) ,
+        );
+    }
+}
